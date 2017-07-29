647b60975e7b7b0c550259da34d1459f4428dcc0
gerodrus <gerodrus@gmail.com>
2017-07-29 11:11:12 +0300

    Added rotation and camera following

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform TargetTransform;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var currentPosition = transform.position;
+        var targetPosition = TargetTransform.position;
+        targetPosition.z = currentPosition.z;
+        var t = 0.2f * Time.deltaTime;
+        transform.position = Vector3.Lerp(currentPosition, targetPosition, t);
+    }
+}
--- Assets/Scripts/Drone/DroneController.cs
+++ Assets/Scripts/Drone/DroneController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DroneController : MonoBehaviour
 {
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var dronePosition = transform.position;
+        var direction = mousePosition - dronePosition;
+        if (Input.GetMouseButton(0))
+        {
+            var currentRotation = transform.rotation;
+            var targetRotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            var t = 0.9f * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, t);
+        }
     }
 }
