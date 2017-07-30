bb88d287bf608a4f61b545ca9c625bccade3ead4
gerodrus <gerodrus@gmail.com>
2017-07-30 15:21:16 +0300

    Added map mode

---
+++ Assets/Scripts/Map/MapSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    float CommonValue;
+    [SerializeField]
+    float MapValue;
+
+    Camera Camera;
+
+    void Awake()
+    {
+        Camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            Camera.orthographicSize = MapValue;
+        }
+        else
+        {
+            Camera.orthographicSize = CommonValue;
+        }
+    }
+}
