76046e2d827aa95e6500eee952ad23fb7eed77d6
gerodrus <gerodrus@gmail.com>
2017-07-30 17:12:58 +0300

    Started to add quests system

---
+++ Assets/Scripts/Cell Modifications/CellQuest.cs
@@ -0,0 +1,13 @@
+﻿public class CellQuest : CellModification
+{
+    public QuestMaster.Quest Quest;
+    public bool AlreadyActivated;
+
+    public override void OnActivate()
+    {
+        if (AlreadyActivated) return;
+        var questMaster = FindObjectOfType<QuestMaster>();
+        questMaster.OnQuestWasCompleted(Quest);
+        AlreadyActivated = true;
+    }
+}
---
+++ Assets/Scripts/Quests/QuestMaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuestMaster : MonoBehaviour
+{
+    public enum Quest
+    {
+        None,
+        SOS,
+    }
+
+    public const int QuestsCount = 1;
+    public List<Quest> CompletedQuests;
+    public Events.Empty OnQuestListWasChanged;
+
+    public void OnQuestWasCompleted(Quest quest)
+    {
+        if (!CompletedQuests.Contains(quest))
+        {
+            CompletedQuests.Add(quest);
+            OnQuestListWasChanged.Invoke();
+        }
+    }
+}
