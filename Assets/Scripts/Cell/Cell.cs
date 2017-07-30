5b48c57be2bc08bf6943fd25f25a182f3c3e97a7
gerodrus <gerodrus@gmail.com>
2017-07-30 19:40:55 +0300

    Added Cell.DrawPath function

--- Assets/Scripts/Cell/Cell.cs
+++ Assets/Scripts/Cell/Cell.cs
@@ -29,6 +29,40 @@ public class Cell : MonoBehaviour
     {
         RefreshRestrictions();
     }
+
+    void OnMouseEnter()
+    {
+//        DrawPath();
+    }
+
+    void OnMouseExit()
+    {
+        DrawPath();
+    }
+
+    void DrawPath()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            var open = Input.GetKey(KeyCode.A);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 cellPosition = transform.position;
+            var vector = mousePosition - cellPosition;
+            var angle = GetAngle(Vector2.right, vector);
+            if (0.0f <= angle && angle < 180.0f / 6.0f || angle <= 0.0f && -180.0f / 6.0f < angle)
+                Data.Right = open;
+            else if (0.0f <= angle && angle < 3.0f * 180.0f / 6.0f)
+                Data.TopRight = open;
+            else if (0.0f <= angle && angle < 5.0f * 180.0f / 6.0f)
+                Data.TopLeft = open;
+            else if (angle <= 0.0f && -3.0f * 180.0f / 6.0f < angle)
+                Data.BottomRight = open;
+            else if (angle <= 0.0f && -5.0f * 180.0f / 6.0f < angle)
+                Data.BottomLeft = open;
+            else 
+                Data.Left = open;
+        }
+    }
 #endif
 
     void OnMouseDown()
@@ -36,6 +70,12 @@ public class Cell : MonoBehaviour
 //        print(this);
         if (!EventSystem.current.IsPointerOverGameObject())
             OnClicked.Invoke(Data.Point.X, Data.Point.Y);
+    }
+    
+    private static float GetAngle(Vector2 v1, Vector2 v2)
+    {
+        var sign = Mathf.Sign(v1.x * v2.y - v1.y * v2.x);
+        return Vector2.Angle(v1, v2) * sign;
     }
 
     public void OnActivate()
