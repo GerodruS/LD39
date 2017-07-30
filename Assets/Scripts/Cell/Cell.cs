494d7118e4e25b1edd4e296b6127039824b75cc4
gerodrus <gerodrus@gmail.com>
2017-07-30 08:53:15 +0300

    Added borders switcher

---
+++ Assets/Scripts/Cell/Cell.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class Cell : MonoBehaviour
+{
+    public bool TopRight;
+    public bool TopLeft;
+    public bool Right;
+    public bool Left;
+    public bool BottomRight;
+    public bool BottomLeft;
+
+    public GameObject TopRightBorder;
+    public GameObject TopLeftBorder;
+    public GameObject RightBorder;
+    public GameObject LeftBorder;
+    public GameObject BottomRightBorder;
+    public GameObject BottomLeftBorder;
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        TopRightBorder.SetActive(TopRight);
+        TopLeftBorder.SetActive(TopLeft);
+        RightBorder.SetActive(Right);
+        LeftBorder.SetActive(Left);
+        BottomRightBorder.SetActive(BottomRight);
+        BottomLeftBorder.SetActive(BottomLeft);
+    }
+#endif
+}
